7d7c0b48047eb51cdcbc76961eed68ddba858fb8
Adnane Win10HP <astit77@gmail.com>
2025-03-01 19:41:05 -0500

    Playing with OOP programming

--- AdnaneConsoleApp1/Program.cs
+++ AdnaneConsoleApp1/Program.cs
@@ -7,6 +7,11 @@ public class HelloJSP_World {
         // Container
     }
 
+    public void Adnane(int i)
+    {
+        Console.WriteLine("The number you input is: " + i);
+    }
+
     public static void Main(String[] args)
     {
         // run stuff here
@@ -16,6 +21,9 @@ public class HelloJSP_World {
 
         Console.WriteLine("yo name is: " + text);
         var text2 = Console.ReadLine();
+
+        HelloJSP_World a = new HelloJSP_World();
+        a.Adnane(10);
     }
 
 }
