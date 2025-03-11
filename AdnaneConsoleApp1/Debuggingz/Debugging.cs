6e5f731037eb28856dce84e71d79ee039382bb17
Adnane Win10HP <astit77@gmail.com>
2025-03-11 14:54:59 -0400

    Re-organized into separate folders and renamed solution to ConsoleApp1

---
+++ AdnaneConsoleApp1/Debuggingz/Debugging.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdnaneConsoleApp1.Debuggingz
+{
+    class Debugging
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to Debugging Demo !");
+
+#if DEBUG
+            Console.WriteLine("!!!!!!! Debug Mode !!!!!!! ");
+#else
+            Console.WriteLine("Release Mode");
+#endif
+
+            // Breakpoint"
+            Console.WriteLine("Setting breakpoint...");
+
+            // Step into, over, and out
+
+        }
+    }
+}
---
+++ AdnaneConsoleApp1/eTA/Program.cs
@@ -0,0 +1,80 @@
+﻿// See https://aka.ms/new-console-template for more information
+// stuff
+
+using OpenQA.Selenium;          // IWebElement
+using OpenQA.Selenium.Chrome;   // new ChromeDriver()
+
+namespace AdnaneConsoleApp1.eTA
+{
+    public class HelloJSP_World
+    {
+        public static void Main(String[] args)
+        {
+            // 3. Selenium WebDriver
+            // ------------------------------
+
+            // 3.1 Create a WebDriver user agent
+            IWebDriver driver = new ChromeDriver();
+            try
+            {
+                // 3.2 Go to URL
+                driver.Navigate().GoToUrl("file:///C:/MyApps/repos/AdnaneConsoleApp1/AdnaneConsoleApp1/web-page.html");
+                //driver.Url = "file:///C:/MyApps/repos/AdnaneConsoleApp1/AdnaneConsoleApp1/web-page.html";
+                driver.Manage().Window.Maximize();
+
+                // 3.3 Selectors - Fill Form
+                IWebElement fname = driver.FindElement(By.Name("fname"));
+                IWebElement lname = driver.FindElement(By.Name("lname"));
+                IWebElement submitBtn = driver.FindElement(By.TagName("button"));
+                IWebElement message = driver.FindElement(By.Id("message"));
+
+                fname.Clear();
+                lname.Clear();
+                fname.SendKeys("Adnane");
+                lname.SendKeys("Stitou");
+                submitBtn.Click();
+
+                // 3.4 Submit Form
+
+                // Check Expected Outputs
+                Console.WriteLine(driver.Title);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Test Error: " + ex.Message);
+            }
+            finally
+            {
+                driver.Quit();
+            }
+
+
+
+            // 2. Use a Method
+            // ------------------------------
+            //HelloJSP_World a = new HelloJSP_World();
+            //a.Adnane(10);
+
+
+            // 1. Read & Write to Console :)
+            // ------------------------------
+            //Console.WriteLine("What is your Name ? ");
+            //var text = Console.ReadLine();
+            //Console.WriteLine("Welcome, " + text + " !!!");
+        }
+
+
+        public HelloJSP_World()
+        {
+            // Container
+        }
+
+        public void Adnane(int i)
+        {
+            Console.WriteLine("The number you input is: " + i);
+        }
+
+    }
+}
+
+
